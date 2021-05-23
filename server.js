8fd4fdce21f524046844b9b6214ab871989fc616
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-05-23 08:45:44 +0530

    Added dockerfile and fixed the proxy issue.

--- server.js
+++ server.js
@@ -4,29 +4,25 @@ const {createProxyMiddleware} = require("http-proxy-middleware")
 
 const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
+const target = process.env.BFF_URL || 'http://localhost:3001'
 const app = next({dev})
 const handle = app.getRequestHandler()
-
-const apiPaths = {
-  '/api': {
-    target: process.env.BFF_URL || 'http://localhost:3001',
-    pathRewrite: {
-      '^/api': '/api'
-    },
-    changeOrigin: true
-  }
-}
 
 app.prepare().then(() => {
   const server = express()
 
-  server.use("/api", createProxyMiddleware(apiPaths['/api']));
+  server.use("/api", (req, res, next) => {
+    if (req.headers.authorization || req.path === "/api/user/sign-in") {
+      return next()
+    }
+    res.status(403).redirect("/login")
+  });
 
-  server.all('*', (req, res) => {
+  server.use('/api', createProxyMiddleware({target, changeOrigin: true, pathRewrite: {'^/api': '/api'},}));
+
+  server.all("*", (req, res) => {
     return handle(req, res)
   })
 
   server.listen(port, () => console.log(`Server is listening on ${port}`))
-}).catch(err => {
-  console.log('Error:::::', err)
 })
