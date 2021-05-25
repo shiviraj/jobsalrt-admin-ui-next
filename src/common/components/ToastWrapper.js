513c1b979d6237f4dc93072b66ba1369e0f9004a
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-05-25 11:00:10 +0530

    fixed toast width and make it as multiline with autohide.

--- src/common/components/ToastWrapper.js
+++ src/common/components/ToastWrapper.js
@@ -2,6 +2,13 @@ import React, {createRef} from 'react';
 import {SnackbarProvider, useSnackbar} from 'notistack';
 import {Close} from "@material-ui/icons";
 import IconButton from "@material-ui/core/IconButton";
+import {makeStyles} from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  toast: {
+    width: theme.spacing(48)
+  }
+}))
 
 const useToast = () => {
   const {enqueueSnackbar} = useSnackbar();
@@ -12,7 +19,7 @@ const useToast = () => {
   }
 
   const info = (message) => {
-    enqueueSnackbar(message, {variant: "info", autoHideDuration: 5000});
+    enqueueSnackbar(message, {variant: "info"});
   }
 
   const error = (message) => {
@@ -20,7 +27,7 @@ const useToast = () => {
   }
 
   const success = (message) => {
-    enqueueSnackbar(message, {variant: "success", autoHideDuration: 5000});
+    enqueueSnackbar(message, {variant: "success"});
   }
 
   return {warning, info, error, success}
@@ -29,12 +36,13 @@ const useToast = () => {
 
 const ToastWrapper = ({children}) => {
   const toastRef = createRef()
+  const classes = useStyles()
   const onClickDismiss = (key) => () => {
     toastRef.current.closeSnackbar(key);
   }
 
-  return <SnackbarProvider maxSnack={5} anchorOrigin={{horizontal: "right", vertical: "top"}} autoHideDuration={null}
-                           ref={toastRef}
+  return <SnackbarProvider maxSnack={5} anchorOrigin={{horizontal: "right", vertical: "top"}} autoHideDuration={5000}
+                           ref={toastRef} aria-multiline={true} className={classes.toast}
                            action={(key) => (<IconButton onClick={onClickDismiss(key)}><Close/></IconButton>)}
   >
     {children}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import {useToast} from "../common/components/ToastWrapper";
 
 const Home = () => {
   const toast = useToast();
-  toast.success("Hello Human, Now you are on Jobsalrt Admin Home page")
+  toast.success("Hello Human, Now you are on Jobsalrt Admin Home page ")
   return (<div>Home</div>);
 };
 
